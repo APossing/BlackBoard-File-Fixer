1fc6cc948db9e7551e70d0589824ea9748b8d1e8
HDatom <atom@possing.com>
2020-02-20 08:37:21 -0800

    more explicit regex

--- Scripts/RenameAssignments.ps1
+++ Scripts/RenameAssignments.ps1
@@ -1,4 +1,4 @@
 param($Extension)
 write-output "Renaming Files"
-Get-ChildItem .\*\* | Where {$_.extension -like $Extension} | Rename-Item -NewName {$_.name -replace '(?s)^.*[0-9]_'}
+Get-ChildItem .\*\* | Where {$_.extension -like $Extension} | Rename-Item -NewName {$_.name -replace '(?s)^.*[-][0-9][0-9]_'}
 write-output "Completed Renaming Files"
