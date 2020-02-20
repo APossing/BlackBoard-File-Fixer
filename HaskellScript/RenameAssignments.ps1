7d2b8b7bced1d1e709d9234d90187ba7bb9cc77b
HDatom <atom@possing.com>
2020-02-19 21:50:24 -0800

    fixing regex

--- HaskellScript/RenameAssignments.ps1
+++ HaskellScript/RenameAssignments.ps1
@@ -1,4 +1,4 @@
-param($extension)
+param($Extension)
 write-output "Renaming Files"
-Get-ChildItem .\*\* | Where {$_.extension -like $Extension} | Rename-Item -NewName {$_.name -replace '(?s)^.*_'}
+Get-ChildItem .\*\* | Where {$_.extension -like $Extension} | Rename-Item -NewName {$_.name -replace '(?s)^.*[0-9]_'}
 write-output "Completed Renaming Files"
